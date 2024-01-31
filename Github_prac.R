a52a45bb9d8ea9936dfd81c9ebf86a8395fbec42
Bria Pierre <pierrebs@vcu.edu>
2024-01-30 22:32:52 -0500

    change point color to green

--- Github_prac.R
+++ Github_prac.R
@@ -7,7 +7,7 @@ library(ggplot2)
 
 #Graphical Representation using mtcars
 ggplot(mtcars, aes(x = mpg, y = disp)) +
-  geom_point(shape = 2, size = 4) +
+  geom_point(color = "blue") +
   labs(title = "Scatter Plot of MPG vs. Disp", x = "Miles Per Gallon", y = "Displacement")
 
 
