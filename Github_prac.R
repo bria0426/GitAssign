fa73ba78a8ff1acd86e2c322e8064e16a82d02c8
Bria Pierre <pierrebs@vcu.edu>
2024-01-30 22:26:25 -0500

    change point and shape

--- Github_prac.R
+++ Github_prac.R
@@ -8,8 +8,8 @@ library(ggplot2)
 #Graphical Representation using mtcars
 ggplot(mtcars, aes(x = mpg, y = disp)) +
   geom_point() +
-  theme_minimal() +
-  labs(title = "Scatter Plot of MPG vs. Disp", x = "Miles Per Gallon", y = "Displacement")
+  labs(title = "Scatter Plot of mpg vs. disp", x = "Miles Per Gallon", y = "Displacement")
+
 
 
 
